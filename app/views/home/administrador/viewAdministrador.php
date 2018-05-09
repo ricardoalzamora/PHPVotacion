619d72aac073769a8ea4b28450751aa83281d22b
Ricardo Alzamora <alzamoraricardo5@gmail.com>
2018-05-08 19:59:59 -0500

    Se implementa modularización en la plantilla.

--- app/views/home/administrador/viewAdministrador.php
+++ app/views/home/administrador/viewAdministrador.php
@@ -1,38 +1,20 @@
 <?php require RUTA_APP . '/views/inc/header.php' ;?>
-    <nav class="center">
+    <nav class="nav">
         <a href="<?php echo RUTA_URL; ?>/Administrador/agregarUsuario/" class="btn">Agregar usuario</a>
+        <a href="<?php echo RUTA_URL; ?>/Administrador/agregarCandidato/" class="btn">Agregar candidato</a>
         <a href="<?php echo RUTA_URL; ?>/Administrador/logOut/" class="btn">Cerrar Sesión</a>
     </nav>
 
-    <table class="table">
-        <thead>
-        <th>Id</th>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Porgrama</th>
-        <th>Acciones</th>
-        </thead>
-        <tbody>
-        <?php foreach ($datos['usuarios'] as $usuario)  :  ?>
-            <tr>
-                <td><?php echo $usuario->id_usuario; ?></td>
-                <td><?php echo $usuario->nombre; ?></td>
-                <td><?php echo $usuario->apellido; ?></td>
-                <td><?php echo $usuario->programa_nombre; ?></td>
-                <td>
-                    <form action="<?php echo RUTA_URL; ?>/Administrador/editarUsuario/" method="get">
-                        <input type="hidden" name="id_usuario" value="<?php echo $usuario->id_usuario; ?>">
-                        <input class="btn" type="submit" value="Editar">
-                    </form>
-                </td>
-                <td>
-                    <form action="<?php echo RUTA_URL; ?>/Administrador/eliminarUsuario/" method="post">
-                        <input type="hidden" name="id_usuario" value="<?php echo $usuario->id_usuario; ?>">
-                        <input class="btn" type="submit" value="Eliminar">
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#div-listUsuarios" role="tab" aria-controls="div-listUsuarios" aria-selected="true">Lista de Usuarios</a>
+        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#div-listCandidatos" role="tab" aria-controls="div-listCandidatos" aria-selected="false">Lista de Candidatos</a>
+    </div>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="div-listUsuarios" role="tabpanel" aria-labelledby="nav-home-tab">
+            <?php require RUTA_APP . '/views/home/administrador/listViewUsuarios.php' ;?>
+        </div>
+        <div class="tab-pane fade" id="div-listCandidatos" role="tabpanel" aria-labelledby="nav-profile-tab">
+            <?php require RUTA_APP . '/views/home/administrador/listViewCandidatos.php' ;?>
+        </div>
+    </div>
 <?php require RUTA_APP . '/views/inc/footer.php' ;?>
